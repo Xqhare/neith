97ce8bbc5135d49a4b6eb02db91116522a9fd1cb
Xqhare <xqhare@web.de>
2023-11-30 19:51:03 +0100

    added logic for collum readout

--- src/jisard.rs
+++ src/jisard.rs
@@ -6,7 +6,19 @@ use json::*;
 
 pub fn main() {
     let aaa = read_json_from_file("test.json");
-    println!("{:?}", aaa);
+    // This makes tables! 
+    for table in aaa.entries() {
+        // table.0 == table name
+        // table.1 == table contents
+        decode_json_table(table.1.clone())
+    }
+}
+
+fn decode_json_table(table_contents: JsonValue) {
+    for entry in table_contents.entries() {
+        println!("{:?}", entry);
+        println!("------------------");
+    }
 }
 
 fn read_json_from_file<P>(filename: P) -> JsonValue where P: AsRef<Path> {
