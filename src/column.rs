07dfbe5b8e33b79f37a1d6408e271f5f87b57211
Xqhare <xqhare@web.de>
2023-12-01 18:13:05 +0100

    removed debug println

--- src/column.rs
+++ src/column.rs
@@ -30,10 +30,8 @@ impl Default for ColumnData {
 impl Column {
     pub fn from_neithdb_column_data(column_value: (&str, &JsonValue)) -> Self {
         let name = column_value.0.to_string();
-        println!("NAME: {:?}", name);
         let data_object = column_value.1.clone();
         let unique = data_object["unique"].as_bool().expect("Boolean not a boolean!");
-        println!("BOOL: {:?}", unique);
         let entry_list = data_object["entry"].members();
         let mut all_row_data: Vec<Data> = Vec::new();
         for thing in entry_list {
@@ -46,7 +44,6 @@ impl Column {
                 all_row_data.push(out);
             }
         }
-        println!("ROWDATA: {:?}", all_row_data);
         return Column{name, unique, contents: ColumnData { all_row_data}};
     }
 }
