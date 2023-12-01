c0c7b20600e901f7e15c71c164d737155bcbe81c
Xqhare <xqhare@web.de>
2023-12-01 17:27:01 +0100

    moved code out into the new files

--- src/jisard.rs
+++ src/jisard.rs
@@ -16,16 +16,6 @@ pub fn main() {
     }
 }
 
-fn decode_json_table(table_contents: JsonValue) {
-    for entry in table_contents.entries() {
-        // println!("{:?}", entry);
-        let test = Column::from_neith_json_column(entry);
-        println!("------------------");
-        println!("{:?}", test);
-        println!("------------------");
-    }
-}
-
 fn read_json_from_file<P>(filename: P) -> JsonValue where P: AsRef<Path> {
     let mut input = File::open(filename).expect("Unable to open file!");
     let mut buffer = String::new();
