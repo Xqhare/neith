326a34a5623976d81bca1c361495d4cb5964e06d
Xqhare <xqhare@web.de>
2023-12-01 17:40:43 +0100

    updated to use tables

--- src/jisard.rs
+++ src/jisard.rs
@@ -4,19 +4,18 @@ use std::{io::Read, fs::File, path::Path};
 
 use json::*;
 
-use crate::column::Column;
+use crate::table::Table;
 
 pub fn main() {
-    let aaa = read_json_from_file("test.neithdb");
+    let aaa = read_json_from_neithdb_file("test.neithdb");
     // This makes tables! 
     for table in aaa.entries() {
-        // table.0 == table name
-        // table.1 == table contents
-        decode_json_table(table.1.clone())
+        let aaa = Table::from_neithdb_table_data(table);
+        println!("AAA: {:?}", aaa);
     }
 }
 
-fn read_json_from_file<P>(filename: P) -> JsonValue where P: AsRef<Path> {
+fn read_json_from_neithdb_file<P>(filename: P) -> JsonValue where P: AsRef<Path> {
     let mut input = File::open(filename).expect("Unable to open file!");
     let mut buffer = String::new();
     let _ = input.read_to_string(&mut buffer);
@@ -24,3 +23,6 @@ fn read_json_from_file<P>(filename: P) -> JsonValue where P: AsRef<Path> {
     return out;
 }
 
+fn write_neithdb_file() {
+    unimplemented!()
+}
