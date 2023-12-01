9c970313d5788f848928430e59b1b6148515858f
Xqhare <xqhare@web.de>
2023-12-01 17:25:54 +0100

    updated to use the new files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 
 // This is the Json-Wizard or Jisard for short.
 mod jisard;
-// The collum representation
+// The column representation
 mod column;
+// The table representation
+mod table;
+// The general data representation
+mod data;
 
 fn main() {
     jisard::main();
