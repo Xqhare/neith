60aabc91e53e30c8c954f1fbc535413d7902cf16
Xqhare <xqhare@web.de>
2023-12-07 19:54:15 +0100

    added get_result() into the API for SuccessMessages

--- src/success.rs
+++ src/success.rs
@@ -5,3 +5,26 @@ pub enum Success {
     SuccessMessage(bool),
     Result(Vec<Data>),
 }
+
+impl Success {
+    /// Function to inspect if Success is wrapping a result.
+    ///
+    /// ## Returns
+    /// Returns `true` if Success is wrapping a result, `false` otherwise.
+    pub fn is_result(&self) -> bool {
+        match self {
+            Success::Result(_data) => true,
+            _ => false,
+        }
+    }
+    /// Function to get the contents of a Success wrapping a result.
+    ///
+    /// ## Returns
+    /// `Some(Vec<Data>)` if a result exists, `None` otherwise.
+    pub fn get_result(&self) -> Option<Vec<Data>> {
+        match self {
+            Success::Result(answ) => return Some(answ.clone()),
+            _ => None,
+        }
+    }
+}
