2a5663cbc1b4d096e0c42b7144417d100a587d03
Xqhare <xqhare@web.de>
2023-12-27 22:01:20 +0100

    reworked tests into an actually useful one

--- src/tests.rs
+++ src/tests.rs
@@ -1,51 +1,67 @@
 use crate::Neith;
 
 #[test]
-fn a_read_neithdb_file() {
-    let test = Neith::connect("test.neithdb");
-    let test_json = Neith::connect("test.json");
-    assert_eq!(test, test_json);
-}
-#[test]
-fn b_test_execute_new() {
-    let mut con = Neith::connect("test.neithdb");
-    let new_table = con.execute("new table testtable with (column1 true, column2 false, column3 false)");
-    let new_column = con.execute("new column testtable with column4 and column5, unique");
-    let new_data_column1 = con.execute("new data testtable (column1 = 1, column2 = -2.04, column3 = true, column4 = text, column5 = (1.04, 2, false, more text))");
-    let new_data_column2 = con.execute("new data testtable (column1 = 2, column2 = -2.04, column3 = true, column4 = text, column5 = (1.04, 2, false, more text))");
-    let new_data_column3 = con.execute("new data testtable (column1 = 3, column2 = 1, column4 = text)");
-    let new_data_column4 = con.execute("new data testtable (column1 = 4, column2 = 1, column4 = text)");
-    assert!(new_column.unwrap() == new_table.unwrap() && new_data_column1.unwrap() == new_data_column2.unwrap() && new_data_column3.unwrap() == new_data_column4.unwrap());
-}
-#[test]
-fn c_test_execute_update() {
-    let mut con = Neith::connect("test.neithdb");
-    let update1 = con.execute("update testtable where [column2 = 1 and column4 = text] with (column3 = true)");
-    let update2 = con.execute("update testtable where [column2 = -2.04 or column2 = 1] with (column3 = false)").unwrap();
-    let update3 = con.execute("update testtable where [column4 = text not column2 = -2.04] with (column5 = (-1, 1.04, true, test text))");
-    assert!(update1.unwrap() == update2 && update2 == update3.unwrap());
-}
-#[test]
-fn d_test_execute_select() {
-    let mut con = Neith::connect("test.neithdb");
-    let select1 = con.execute("select * from testtable");
-    let select2 = con.execute("select (column1, column2, column3, column4) from testtable");
-    assert!(select1.unwrap() == select2.unwrap());
-}
-#[test]
-fn e_test_execute_get() {
-    let mut con = Neith::connect("test.neithdb");
-    let get_min = con.execute("get min in column1 from testtable");
-    let get_max = con.execute("get max on column1 from testtable").unwrap();
-    let get_len = con.execute("get len of testtable").unwrap();
-    // len and max should? be the same length later too!
-    assert!(get_len == get_max && get_max == get_min.unwrap());
-}
-#[test]
-fn f_test_execute_delete() {
-    let mut con = Neith::connect("test.neithdb");
-    let _del_row = con.execute("delete data in testtable where [column1 = 4 and column4 = text]");
-    let del_column = con.execute("delete column with column5 and column4 in testtable");
-    let del_table = con.execute("delete table with testtable");
-    assert!(del_column.unwrap() == del_table.unwrap());
+fn test_main_connection() {
+    // CONST:
+    let names = ["Joe Murica", "Mae Nada", "Hesus Xico", "Hermann Man", "Amelie Ance", "Mario Taly", "Maria Pain", "Peter Port", "Matt Ain", "Dirk Ands", "Dennis Mark", "Vitrali Pol"];
+    let jobs = ["Handyman", "Secretary", "Salesman", "Manager", "Cook", "Programmer", "Designer", "Artist", "CEO", "COO", "CFO", "CPO"];
+    let city = ["New York", "Montreal", "Mexico City", "Stuttgart", "Paris", "Siena", "Barcelona", "Lisbon", "Hull", "Amsterdam", "Copenhagen", "Gdansk"];
+    let gender = ["male", "female", "male", "male", "female", "male", "female", "male", "male", "male", "male", "male"];
+    let salary = [1500, 2500, 3000, 1250, 500, 6574, 5858, 9687, 6800, 5970, 5570, 4780];
+    let seniority = [1, 4, 8, 4, 5, 14, 19, 2, 24, 3, 16, 41];
+    let fav_colour = ["red", "green", "blue", "yellow", "red", "green", "blue", "yellow", "red", "green", "blue", "yellow"];
+    // Neith connection, set up and table creation
+    let mut con = Neith::connect("test");
+    let _change_marker = con.set_marker(",");
+    let _activate_history = con.set_job_history(true);
+    let first_table = con.execute("new table testtable with (id true, full_name false, gender false, city false, job false)");
+    let add_column = con.execute("new column testtable with (salary false, seniority false, colour false)");
+    assert_eq!(first_table.unwrap(), add_column.unwrap());
+    // Data creation (12 times!)
+    for employee in names.iter().enumerate() {
+        let id = employee.0;
+        let name = employee.1;
+        let gender = gender[id];
+        let city = city[id];
+        let job = jobs[id];
+        let salary = salary[id];
+        let seniority = seniority[id];
+        let colour = fav_colour[id];
+        // remember, split pattern is set to ","!
+        let cmd = format!("new data testtable (id = {id}, full_name = {name}, gender = {gender}, city = {city}, job = {job}, salary = {salary}, seniority = {seniority}, colour = {colour})");
+        let _wirte_data = con.execute(&cmd);
+    }
+    // let _ = con.clone().save();
+    // Data fetching and updating
+    let to_change_ids_read = con.execute("select (id) from testtable where [gender = male, and colour = red]");
+    let change_ids = to_change_ids_read.unwrap().get_result().unwrap()[0].get_list().unwrap();
+    for id in change_ids {
+        let decoded_id = id.get_float().unwrap();
+        let _update0 = con.execute(format!("update testtable where [id = {decoded_id}] with (city = Bejing)").as_str());
+        let changed_city = con.execute(format!("select (city) from testtable where [id = {decoded_id}]").as_str()).unwrap();
+        let read_city = changed_city.get_result().unwrap()[0].get_list().unwrap();
+        for city in read_city {
+            let city_name = city.get_string().unwrap();
+            assert_eq!(city_name, "Bejing".to_string());
+        }
+    }
+    let all_employee_data = con.execute("select * from testtable").unwrap();
+    let read_data = all_employee_data.get_result().unwrap();
+    for column in read_data {
+        let this_column = column.get_list().unwrap();
+        assert_eq!(this_column.len(), 12);
+    }
+    // Getters
+    let min_id = con.execute("get min in id from testtable").unwrap().get_result().unwrap()[0].get_float().unwrap();
+    assert_eq!(min_id, 0.0);
+    let max_id = con.execute("get max in id from testtable").unwrap().get_result().unwrap()[0].get_float().unwrap();
+    assert_eq!(max_id, 11.0);
+    let len_table = con.execute("get len of testtable").unwrap().get_result().unwrap()[0].get_float().unwrap();
+    assert_eq!(len_table, 12.0);
+    // Reseting file for next test
+    let del_data = con.execute("delete data in testtable where [gender = male, or city = Paris]").unwrap();
+    let del_column = con.execute("delete column with seniority in testtable").unwrap();
+    let del_table = con.execute("delete table with testtable").unwrap();
+    assert_eq!(del_data, del_column);
+    assert_eq!(del_column, del_table);
 }
